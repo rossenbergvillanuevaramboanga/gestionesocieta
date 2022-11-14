8ad71e4c68d72c63e9df506098410f2b1366aaf6
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-14 14:02:08 +0100

    First Commit

---
+++ src/main/java/it/prova/gestionesocieta/GestionesocietaApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionesocieta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionesocietaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionesocietaApplication.class, args);
+	}
+
+}
